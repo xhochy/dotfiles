838292db7b9377a10ac27c45e34f7f8871473c29
Uwe L. Korn <uwelk@xhochy.com>
2012-03-28 23:15:10 +0200

    Add missing libs fpr nodejs

--- install.sh
+++ install.sh
@@ -27,7 +27,11 @@ CURRENT_NODEJS_VERSION=`cat "$DOTFILES_DIR/nodejs/CURRENT_VERSION"`
 nvm install v$CURRENT_NODEJS_VERSION
 nvm use v$CURRENT_NODEJS_VERSION
 if [ "x$NVM_BIN" = "x" ]; then
+    wget http://tulip.hadiko.de/xhochy/nodejs/build/amd64/libssl.so.1.0.0 -O $DOTFILES_DIR/nodejs/lib/libssl.so.1.0.0
+    wget http://tulip.hadiko.de/xhochy/nodejs/build/amd64/libcrypto.so.1.0.0 -O $DOTFILES_DIR/nodejs/lib/libcrypto.so.1.0.0
     wget http://tulip.hadiko.de/xhochy/nodejs/build/amd64/node-$CURRENT_NODEJS_VERSION -O $DOTFILES_DIR/nodejs/bin/node
+    chmod +x DOTFILES_DIR/nodejs/bin/node
+    export LD_LIBRARY_PATH="$DOTFILES_DIR/nodejs/lib:$LD_LIBRARY_PATH"
     export PATH="$DOTFILES_DIR/nodejs/bin:$PATH"
     curl http://npmjs.org/install.sh | sh
 fi
