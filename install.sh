b4f1a82f8900f6234090c0e7530a880eb01eccbe
Uwe L. Korn <uwelk@xhochy.com>
2012-03-28 23:26:43 +0200

    Remove static binary installation of nodejs

--- install.sh
+++ install.sh
@@ -26,16 +26,6 @@ source "$DOTFILES_DIR/3rdparty/nvm.git/nvm.sh"
 CURRENT_NODEJS_VERSION=`cat "$DOTFILES_DIR/nodejs/CURRENT_VERSION"`
 nvm install v$CURRENT_NODEJS_VERSION
 nvm use v$CURRENT_NODEJS_VERSION
-if [ "x$NVM_BIN" = "x" ]; then
-    wget http://tulip.hadiko.de/xhochy/nodejs/build/amd64/libssl.so.1.0.0 -O $DOTFILES_DIR/nodejs/lib/libssl.so.1.0.0
-    wget http://tulip.hadiko.de/xhochy/nodejs/build/amd64/libcrypto.so.1.0.0 -O $DOTFILES_DIR/nodejs/lib/libcrypto.so.1.0.0
-    wget http://tulip.hadiko.de/xhochy/nodejs/build/amd64/libc-2.15.so -O $DOTFILES_DIR/nodejs/lib/libc.so.6
-    wget http://tulip.hadiko.de/xhochy/nodejs/build/amd64/node-$CURRENT_NODEJS_VERSION -O $DOTFILES_DIR/nodejs/bin/node
-    chmod +x DOTFILES_DIR/nodejs/bin/node
-    export LD_LIBRARY_PATH="$DOTFILES_DIR/nodejs/lib:$LD_LIBRARY_PATH"
-    export PATH="$DOTFILES_DIR/nodejs/bin:$PATH"
-    curl http://npmjs.org/install.sh | sh
-fi
 
 # Setup CoffeeScript and other dependencies
 npm install -g coffee-script
